1e5d4125da5becdbc0c64de3b6f0f9dc2591b9d5
Dana Vlas <dana-monica.vlas@edu.fh-joanneum.at>
2023-06-11 22:09:50 +0200

    adding the template log4j2

--- src/main/java/at/fhj/msd/Calculator.java
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -1,28 +1,43 @@
 package at.fhj.msd;
 
-public class Calculator {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+public class Calculator {
+    private static Logger logger = LogManager.getLogger();
     public double add(double number1, double number2){
         double sum = number1 + number2;
+        logger.error("You have called with " + number1 + " , and " + number2); // nu uita aici debug in loc de error
         return sum;
     }
 
     public double minus(double number1, double number2){
         double dif = number1 - number2;
+        logger.error("You have called with " + number1 + " , and " + number2); // nu uita aici debug in loc de error
         return dif;
     }
 
     public double divide(double number1, double number2){
-        double div = number1 / number2;
-        return div;
+        logger.error("You have called with " + number1 + " , and " + number2);  // nu uita aici debug in loc de error
+        if(number2 == 0){
+            logger.error("We can not divide with 0");
+            return 0;
+        }else{
+            double div = number1 / number2;
+            return div;
+        }
+
+
     }
 
     public double multiply(double number1, double number2){
         double produkt = number1 * number2;
+        logger.error("You have called with " + number1 + " , and " + number2); // nu uita aici debug in loc de error
         return produkt;
     }
     public double calcFaculty(double number){
         double faculty = 1;
+        logger.error("You have called with " + number );   // nu uita aici debug in loc de error
         while(number > 1){
             faculty *= number--;
         }
--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,14 +1,23 @@
 package at.fhj.msd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 public class Main {
+    private static Logger logger = LogManager.getLogger();
     public static void main(String[] args) {
         Calculator calc = new Calculator();
+
+
+
+        logger.info("It is a info logger.");
 
         System.out.println("Sum: " + calc.add(3,7));
         System.out.println("Dif: " + calc.minus(10,3));
         System.out.println("Product: " + calc.multiply(5,5));
         System.out.println("Division: " + calc.divide(10,5));
-
+        logger.error("It is an error logger.");
 
     }
 }
